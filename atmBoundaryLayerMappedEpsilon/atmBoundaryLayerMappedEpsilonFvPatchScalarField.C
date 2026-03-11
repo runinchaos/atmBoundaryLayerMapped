b3589da57905ab324118d9aa99cc0805ccf93fef
RZHAO <zhaorm@tju.edu.cn>
2026-03-11 18:14:02 +0800

    feat: Initial fork of OpenFOAM atmBoundaryLayer family with renamed classes
    - Renamed atmBoundaryLayer -> atmBoundaryLayerMapped
    - Renamed atmBoundaryLayerInletVelocity -> atmBoundaryLayerMappedVelocity
    - Renamed atmBoundaryLayerInletK -> atmBoundaryLayerMappedK
    - Renamed atmBoundaryLayerInletEpsilon -> atmBoundaryLayerMappedEpsilon
    - Renamed atmBoundaryLayerInletOmega -> atmBoundaryLayerMappedOmega
    - Compiles successfully with renamed classes only (no functional changes yet)

---
+++ atmBoundaryLayerMappedEpsilon/atmBoundaryLayerMappedEpsilonFvPatchScalarField.C
@@ -0,0 +1,171 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2018 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "atmBoundaryLayerMappedEpsilonFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "turbulenceModel.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+atmBoundaryLayerMappedEpsilonFvPatchScalarField::
+atmBoundaryLayerMappedEpsilonFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletFvPatchScalarField(p, iF),
+    atmBoundaryLayerMapped(iF.time(), p.patch())
+{}
+
+
+atmBoundaryLayerMappedEpsilonFvPatchScalarField::
+atmBoundaryLayerMappedEpsilonFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    inletOutletFvPatchScalarField(p, iF),
+    atmBoundaryLayerMapped(iF.time(), p.patch(), dict)
+{
+    phiName_ = dict.getOrDefault<word>("phi", "phi");
+
+    refValue() = epsilon(patch().Cf());
+    refGrad() = 0;
+    valueFraction() = 1;
+
+    if (!initABL_)
+    {
+        scalarField::operator=(scalarField("value", dict, p.size()));
+    }
+    else
+    {
+        scalarField::operator=(refValue());
+        initABL_ = false;
+    }
+}
+
+
+atmBoundaryLayerMappedEpsilonFvPatchScalarField::
+atmBoundaryLayerMappedEpsilonFvPatchScalarField
+(
+    const atmBoundaryLayerMappedEpsilonFvPatchScalarField& psf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    inletOutletFvPatchScalarField(psf, p, iF, mapper),
+    atmBoundaryLayerMapped(psf, p, mapper)
+{}
+
+
+atmBoundaryLayerMappedEpsilonFvPatchScalarField::
+atmBoundaryLayerMappedEpsilonFvPatchScalarField
+(
+    const atmBoundaryLayerMappedEpsilonFvPatchScalarField& psf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletFvPatchScalarField(psf, iF),
+    atmBoundaryLayerMapped(psf)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void atmBoundaryLayerMappedEpsilonFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    refValue() = epsilon(patch().Cf());
+
+    inletOutletFvPatchScalarField::updateCoeffs();
+}
+
+
+void atmBoundaryLayerMappedEpsilonFvPatchScalarField::autoMap
+(
+    const fvPatchFieldMapper& m
+)
+{
+    inletOutletFvPatchScalarField::autoMap(m);
+    atmBoundaryLayerMapped::autoMap(m);
+}
+
+
+void atmBoundaryLayerMappedEpsilonFvPatchScalarField::rmap
+(
+    const fvPatchScalarField& psf,
+    const labelList& addr
+)
+{
+    inletOutletFvPatchScalarField::rmap(psf, addr);
+
+    const atmBoundaryLayerMappedEpsilonFvPatchScalarField& blpsf =
+        refCast<const atmBoundaryLayerMappedEpsilonFvPatchScalarField>(psf);
+
+    atmBoundaryLayerMapped::rmap(blpsf, addr);
+}
+
+
+void atmBoundaryLayerMappedEpsilonFvPatchScalarField::write(Ostream& os) const
+{
+    fvPatchScalarField::write(os);
+    os.writeEntryIfDifferent<word>("phi", "phi", phiName_);
+    atmBoundaryLayerMapped::write(os);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField
+(
+    fvPatchScalarField,
+    atmBoundaryLayerMappedEpsilonFvPatchScalarField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
---
+++ atmBoundaryLayerMappedVelocity/atmBoundaryLayerMappedVelocityFvPatchVectorField.C
@@ -0,0 +1,170 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2018 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "atmBoundaryLayerMappedVelocityFvPatchVectorField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+atmBoundaryLayerMappedVelocityFvPatchVectorField::
+atmBoundaryLayerMappedVelocityFvPatchVectorField
+(
+    const fvPatch& p,
+    const DimensionedField<vector, volMesh>& iF
+)
+:
+    inletOutletFvPatchVectorField(p, iF),
+    atmBoundaryLayerMapped(iF.time(), p.patch())
+{}
+
+
+atmBoundaryLayerMappedVelocityFvPatchVectorField::
+atmBoundaryLayerMappedVelocityFvPatchVectorField
+(
+    const fvPatch& p,
+    const DimensionedField<vector, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    inletOutletFvPatchVectorField(p, iF),
+    atmBoundaryLayerMapped(iF.time(), p.patch(), dict)
+{
+    phiName_ = dict.getOrDefault<word>("phi", "phi");
+
+    refValue() = U(patch().Cf());
+    refGrad() = Zero;
+    valueFraction() = 1;
+
+    if (!initABL_)
+    {
+        vectorField::operator=(vectorField("value", dict, p.size()));
+    }
+    else
+    {
+        vectorField::operator=(refValue());
+        initABL_ = false;
+    }
+}
+
+
+atmBoundaryLayerMappedVelocityFvPatchVectorField::
+atmBoundaryLayerMappedVelocityFvPatchVectorField
+(
+    const atmBoundaryLayerMappedVelocityFvPatchVectorField& pvf,
+    const fvPatch& p,
+    const DimensionedField<vector, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    inletOutletFvPatchVectorField(pvf, p, iF, mapper),
+    atmBoundaryLayerMapped(pvf, p, mapper)
+{}
+
+
+atmBoundaryLayerMappedVelocityFvPatchVectorField::
+atmBoundaryLayerMappedVelocityFvPatchVectorField
+(
+    const atmBoundaryLayerMappedVelocityFvPatchVectorField& pvf,
+    const DimensionedField<vector, volMesh>& iF
+)
+:
+    inletOutletFvPatchVectorField(pvf, iF),
+    atmBoundaryLayerMapped(pvf)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void atmBoundaryLayerMappedVelocityFvPatchVectorField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    refValue() = U(patch().Cf());
+
+    inletOutletFvPatchVectorField::updateCoeffs();
+}
+
+
+void atmBoundaryLayerMappedVelocityFvPatchVectorField::autoMap
+(
+    const fvPatchFieldMapper& m
+)
+{
+    inletOutletFvPatchVectorField::autoMap(m);
+    atmBoundaryLayerMapped::autoMap(m);
+}
+
+
+void atmBoundaryLayerMappedVelocityFvPatchVectorField::rmap
+(
+    const fvPatchVectorField& pvf,
+    const labelList& addr
+)
+{
+    inletOutletFvPatchVectorField::rmap(pvf, addr);
+
+    const atmBoundaryLayerMappedVelocityFvPatchVectorField& blpvf =
+        refCast<const atmBoundaryLayerMappedVelocityFvPatchVectorField>(pvf);
+
+    atmBoundaryLayerMapped::rmap(blpvf, addr);
+}
+
+
+void atmBoundaryLayerMappedVelocityFvPatchVectorField::write(Ostream& os) const
+{
+    fvPatchVectorField::write(os);
+    os.writeEntryIfDifferent<word>("phi", "phi", phiName_);
+    atmBoundaryLayerMapped::write(os);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField
+(
+    fvPatchVectorField,
+    atmBoundaryLayerMappedVelocityFvPatchVectorField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
